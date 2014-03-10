ee2ade44db73f9bfb49b6475f0633d053d283262
Mark Nemec <mrknmc@me.com>
2014-03-10 23:48:13 +0000

    got the direct mapped cache working

--- car_cw2/cache.py
+++ car_cw2/cache.py
@@ -1,6 +1,8 @@
 # Memory addresses are 48 bits long.
 # Data in memory is organized in blocks of 32 bytes.
 # Assume a write through cache with LRU replacement policy for set associative caches.
+from sys import argv
+from math import log
 
 
 def parse(file):
@@ -11,12 +13,37 @@ def parse(file):
     file.seek(0)  # reset file pointer
 
 
-def direct(file, cache_size=4096, block_size=32, sets=1):
+def set_associative(file, ):
+    """"""
+    pass
+
+
+def direct_mapped(file, cache_size=4096, block_size=32):
     """Uses a direct-mapped cache with the simulator."""
     cache = {}
-    cur_size = 0
+    misses = {'R': 0, 'W': 0}
+
+    offset_size = int(log(block_size, 2))  # need 5 bits for 32 bytes
+    index_size = int(log(cache_size / float(block_size), 2))
+
     for op, addr in parse(file):
-        addr = decode(addr)
+        tag, index, offset = decode(addr, offset_size, index_size)
+
+        if index not in cache:
+            # not in cache - add it
+            misses[op] += 1
+            cache[index] = tag
+        else:
+            if cache[index] != tag:
+                # different tag - replace it
+                misses[op] += 1
+                cache[index] = tag
+            else:
+                if op == 'W':
+                    # replace the value
+                    cache[index] = tag
+
+    return misses
 
 
 def decode(addr, offset_size=8, index_size=7):
@@ -36,7 +63,12 @@ def decode(addr, offset_size=8, index_size=7):
     return tag, index, offset
 
 
-def main(filename=None):
-
+def main(filename):
+    """"""
     with open(filename) as file:
-        pass
+        print direct_mapped(file)
+
+
+if __name__ == '__main__':
+    # parser = ArgumentParser(description='Run branch predictors on a given trace file.')
+    main(filename=argv[1])
