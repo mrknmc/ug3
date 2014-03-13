f67e4edaacc32e89df60848e06730f167961a475
Mark Nemec <mrknmc@me.com>
2014-03-13 18:08:25 +0000

    fixing receiver2 with last packet

--- comn/src/Receiver2.java
+++ comn/src/Receiver2.java
@@ -7,24 +7,64 @@ import java.net.DatagramSocket;
 import java.nio.ByteBuffer;
 
 
-public class Receiver2 extends Receiver1 {
-
+public class Receiver2 {
+    public static final int MSG_SIZE = 1024;
+    public static final int HEADER_SIZE = 3;
+    private DatagramSocket socket;
+    private FileOutputStream outStream;
     private int curACK = 0;
     private int curSeq = -1;
 
-    public Receiver2(String[] args) throws IOException {
-        super(args);
+    /**
+     * Constructs a Receiver1 object from the given properties.
+     *
+     * @param port     port number the receiver listens on.
+     * @param fileName name of the file the receiver writes to.
+     * @throws IOException
+     */
+    public Receiver2(int port, String fileName) throws IOException {
+        this.socket = new DatagramSocket(port);
+        this.outStream = new FileOutputStream(fileName);
     }
 
-    public static void main(String[] args) {
+    /**
+     * Constructs a Receiver object from properties encoded in a String array.
+     *
+     * @param args arguments of the Receiver1
+     * @throws IOException
+     */
+    public Receiver2(String[] args) throws IOException {
+        this(Integer.parseInt(args[0]), args[1]);
+    }
+
+    /**
+     * Returns true if the arguments are valid for this receiver.
+     *
+     * @param args arguments to be validated.
+     * @return the validity of the arguments.
+     */
+    public static boolean validateArgs(String[] args) {
+        if (args.length < 2) {
+            if (args.length < 1) {
+                System.err.println("No port number specified!");
+            } else {
+                System.err.println("No file name specified!");
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
         if (!validateArgs(args)) {
             System.exit(1);
         }
-        Receiver2 receiver = null;
 
+        Receiver2 receiver = null;
         try {
             receiver = new Receiver2(args);
             receiver.receive();
+            System.out.println("File received.");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -34,20 +74,13 @@ public class Receiver2 extends Receiver1 {
         }
     }
 
-    public int getCurACK() {
-        return curACK;
-    }
-
-    public void setCurACK(int curACK) {
-        this.curACK = curACK;
-    }
-
-    public int getCurSeq() {
-        return curSeq;
-    }
-
-    public void setCurSeq(int curSeq) {
-        this.curSeq = curSeq;
+    /**
+     * Closes all of the running transactions etc.
+     */
+    protected void close() throws IOException {
+        socket.close();
+        outStream.flush();
+        outStream.close();
     }
 
     /**
@@ -55,49 +88,56 @@ public class Receiver2 extends Receiver1 {
      *
      * @param receivedPacket packet we are acknowledging.
      */
-    protected void sendACK(DatagramPacket receivedPacket) {
-        DatagramSocket socket = getSocket();
+    protected void sendACK(DatagramPacket receivedPacket) throws IOException {
         DatagramPacket packet = makeACKPacket(receivedPacket);
-        try {
-            System.out.printf("Sending ACK %d\n", curACK);
-            socket.send(packet);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        socket.send(packet);
+        System.out.printf("Sent ACK %d.\n", curACK);
     }
 
-    @Override
+    public int getMsgSize() {
+        return MSG_SIZE - HEADER_SIZE;
+    }
+
+    public int getTotalSize() {
+        return MSG_SIZE;
+    }
+
     protected int extractPacket(DatagramPacket packet, ByteBuffer dest) {
-        int sequence = super.extractPacket(packet, dest);
-        if (sequence == getCurSeq()) {
+        dest.clear();
+        byte[] data = packet.getData();
+        int sequence = ByteBuffer.wrap(new byte[]{0, 0, data[1], data[0]}).getInt();
+        byte eof = data[2];
+        dest.put(data, HEADER_SIZE, packet.getLength() - HEADER_SIZE);
+        if (sequence == curSeq) {
             return -2;
         }
         curSeq = sequence;
-        return sequence;
+        return eof == 1 ? -1 : sequence;
     }
 
-    @Override
     protected void receive() throws IOException {
         byte[] buf = new byte[getTotalSize()];
-        DatagramSocket socket = getSocket();
-        FileOutputStream outStream = getOutStream();
         ByteBuffer packetData = ByteBuffer.allocate(getMsgSize());
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
         int sequence = 0;
 
-        while (sequence != -1) {
+        while (true) {
             socket.receive(packet);
-            while (true) {
-                sequence = extractPacket(packet, packetData);
-                if (sequence != -2) {
-                    System.out.printf("Received packet %d\n", sequence);
-                    sendACK(packet);
-                    curACK = curACK == 0 ? 1 : 0;
+            sequence = extractPacket(packet, packetData);
+            System.out.printf("Received packet %d.\n", sequence);
+            if (sequence == -2) {
+                // Repeated packet
+                System.out.printf("Received duplicate packet %d.\n", sequence);
+            } else {
+                sendACK(packet);
+                if (sequence == -1) {
                     break;
+                } else {
+                    // Only write data if not last packet
+                    curACK = curACK == 0 ? 1 : 0;
+                    outStream.write(packetData.array());
                 }
-                System.out.printf("Received duplicate packet %d\n", sequence);
             }
-            outStream.write(packetData.array());
         }
     }
 
@@ -108,7 +148,7 @@ public class Receiver2 extends Receiver1 {
      * @return packet to be sent as acknowledgement
      */
     protected DatagramPacket makeACKPacket(DatagramPacket receivedPacket) {
-        byte[] data = {(byte) getCurACK()};
+        byte[] data = {(byte) curACK};
         return new DatagramPacket(data, data.length, receivedPacket.getAddress(), receivedPacket.getPort());
     }
 
--- comn/src/Sender2.java
+++ comn/src/Sender2.java
@@ -1,42 +1,134 @@
 /* Mark Nemec s1140740 */
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
 
 
-public class Sender2 extends Sender1 {
-
+public class Sender2 {
+    public static final int MSG_SIZE = 1024;
+    public static final int HEADER_SIZE = 3;
     public static final int DEFAULT_TIMEOUT = 2000;
+    private InetAddress address;
+    private int port;
+    private DatagramSocket socket;
+    private FileInputStream inStream;
     private int curACK = 0;
 
-    public Sender2(String[] args) {
-        super(args);
+    /**
+     * Constructs a Sender1 object with given properties.
+     *
+     * @param hostName the address of the server.
+     * @param port     the port of the server.
+     * @param fileName the file name of the file to transfer.
+     */
+    public Sender2(String hostName, int port, String fileName) throws IOException {
+        this.address = InetAddress.getByName(hostName);
+        this.port = port;
+        this.socket = new DatagramSocket();
+        this.inStream = new FileInputStream(fileName);
     }
 
-    public static void main(String[] args) {
+    /**
+     * Constructs a Sender1 object with properties encoded in an array of Strings;
+     *
+     * @param args arguments of the Sender1
+     */
+    public Sender2(String[] args) throws IOException {
+        this(args[0], Integer.parseInt(args[1]), args[2]);
+    }
+
+    public static void main(String[] args) throws IOException {
         if (!validateArgs(args)) {
             System.exit(1);
         }
-
-        Sender2 sender = new Sender2(args);
+        Sender2 sender = null;
         try {
+            sender = new Sender2(args);
             sender.send();
+            System.out.println("File sent.");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            sender.close();
+            if (sender != null) {
+                sender.close();
+            }
         }
-        System.out.println("File sent.");
     }
 
-    public int getCurACK() {
-        return curACK;
+    /**
+     * Converts an int into a specified number of bytes
+     *
+     * @param value      integer value to convert
+     * @param numOfBytes how many bytes to use for the int
+     * @return int converted into a byte array
+     */
+    public static byte[] intToBytes(int value, int numOfBytes) {
+        byte[] byteArray = new byte[numOfBytes];
+        for (int i = 0; i < numOfBytes; i++) {
+            byteArray[i] = (byte) ((value >> 8 * i) & 0xFF);
+        }
+        return byteArray;
     }
 
-    public void setCurAck(int newAck) {
-        this.curACK = newAck;
+    /**
+     * Returns true if the arguments are valid for this sender.
+     *
+     * @param args arguments to be validated.
+     * @return the validity of the arguments.
+     */
+    public static boolean validateArgs(String[] args) {
+        if (args.length < 3) {
+            if (args.length < 1) {
+                System.err.println("No host name specified!");
+            } else if (args.length < 2) {
+                System.err.println("No port number specified!");
+            } else {
+                System.err.println("No file name specified!");
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Reads a file and sends packets to the receiver.
+     *
+     * @throws IOException
+     */
+    public void send() throws IOException {
+        DatagramPacket packet;
+        byte[] byteArray = new byte[getMsgSize()];
+        boolean fileRead = false;
+        int counter = 0;
+
+        while (!fileRead) {
+            fileRead = inStream.read(byteArray) == -1;
+            packet = makePacket(byteArray, counter, fileRead);
+            sendPacket(packet);
+            System.out.printf("Sent packet %d\n", counter);
+            counter++;
+        }
+    }
+
+    /**
+     * Closes all of the running transactions etc.
+     */
+    public void close() throws IOException {
+        socket.close();
+        inStream.close();
+    }
+
+    protected int getMsgSize() {
+        return MSG_SIZE - HEADER_SIZE;
+    }
+
+    int getTotalSize() {
+        return MSG_SIZE;
     }
 
     /**
@@ -46,11 +138,10 @@ public class Sender2 extends Sender1 {
      * @throws IOException
      */
     public int waitForACK(int timeout) throws IOException {
-        DatagramSocket socket = getSocket();
         byte[] buf = new byte[1];
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
-
         socket.setSoTimeout(timeout);
+
         while (true) {
             socket.receive(packet);
             int recACK = (int) packet.getData()[0];
@@ -62,18 +153,41 @@ public class Sender2 extends Sender1 {
         }
     }
 
-    @Override
+    /**
+     * Send an individual packet.
+     *
+     * @param packet packet to be sent
+     * @throws IOException
+     */
     protected void sendPacket(DatagramPacket packet) throws IOException {
-        DatagramSocket socket = getSocket();
         while (true) {
             try {
                 socket.send(packet);
                 waitForACK(DEFAULT_TIMEOUT);
                 break;
             } catch (SocketTimeoutException e) {
-                System.out.printf("Timed out waiting for ACK %d\n", getCurACK());
+                System.out.printf("Timed out waiting for ACK %d\n", curACK);
             }
         }
+    }
+
+    /**
+     * Creates the packet to be sent. This contains the sequence number,
+     * whether we are sending the last packet and the data array.
+     *
+     * @param data     message data that we are sending.
+     * @param sequence sequence number sent in the header.
+     * @param fileRead whether this is the last packet to be sent.
+     * @return packet to be sent to the receiver.
+     */
+    public DatagramPacket makePacket(byte[] data, int sequence, boolean fileRead) {
+        ByteBuffer buffer = ByteBuffer.allocate(getTotalSize());
+        byte eof = fileRead ? (byte) 1 : (byte) 0;
+        byte[] byteSequence = intToBytes(sequence, 2);
+        buffer.put(byteSequence);
+        buffer.put(eof);
+        buffer.put(data);
+        return new DatagramPacket(buffer.array(), 0, buffer.capacity(), this.address, this.port);
     }
 
 }
