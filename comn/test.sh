6e836e33043da4e80dcbd65b79b7e30a0ecfc065
Mark Nemec <mrknmc@me.com>
2014-03-17 01:29:58 +0000

    updated tests

--- comn/test.sh
+++ comn/test.sh
@@ -10,7 +10,6 @@ ipfw add pipe 200 out
 ipfw pipe 100 config delay 10ms plr 0.05 bw 10Mbits/s
 ipfw pipe 200 config delay 10ms plr 0.05 bw 10Mbits/s
 
-
 echo timeout, retransmission, throughput
 
 for TIMEOUT in 10 20 30 40 50 60 70 80 90 100
@@ -25,13 +24,14 @@ done
 # Question 3
 # 1% packet loss rate 10Mbps bandwith and variable delay
 
-echo window, delay, retransmission, throughput
+echo window, delay, throughput
 
 for DELAY in 10 100 300
 do
         ipfw flush
         ipfw add pipe 100 in
         ipfw add pipe 200 out
+
         ipfw pipe 100 config delay ${DELAY}ms plr 0.005 bw 10Mbits/s
         ipfw pipe 200 config delay ${DELAY}ms plr 0.005 bw 10Mbits/s
 
@@ -41,7 +41,26 @@ do
                 echo -n "$WINDOW, $DELAY, "
                 java Receiver3 54321 rfile.jpg > /dev/null &
                 sleep 1
-                java Sender3 localhost 54321 sfile.jpg &retrytimeout $WINDOW
+                java Sender3 localhost 54321 sfile.jpg $TIMEOUT $WINDOW
         done
 done
 
+# Question 5
+# 1% packet loss rate 10Mbps bandwidth and 100ms delay
+
+ipfw flush
+ipfw add pipe 100 in
+ipfw add pipe 200 out
+
+ipfw pipe 100 config delay 100ms plr 0.005 bw 10Mbits/s
+ipfw pipe 200 config delay 100ms plr 0.005 bw 10Mbits/s
+
+echo window, throughput
+
+for WINDOW in 8 16 32 64 128 256
+do
+        echo -n "$WINDOW, "
+        java Receiver4 54321 rfile.jpg > /dev/null &
+        sleep 1
+        java Sender4 localhost 54321 sfile.jpg  $WINDOW
+done
